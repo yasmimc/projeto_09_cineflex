3485cd4836294916f9f68d0391517064e426750f
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-09-04 14:58:58 -0300

    Added back to home btn

--- src/components/Sucess/Sucess.js
+++ src/components/Sucess/Sucess.js
@@ -1,33 +1,36 @@
+import { Link } from "react-router-dom";
 import "../Sucess/Sucess.css"
 
-export default function Sucess(props){
+export default function Sucess(props) {
     const {
         movie,
         buyer,
-        session, 
+        session,
         seats
     } = props.confirmedBooking;
-    
-    return(
+
+    return (
         <div className="Sucess">
             <h2>Pedido feito com sucesso!</h2>
-            
+
             <h3>Filme e sessão</h3>
             <p>{movie.title}</p>
             <p>{session.day.weekday} {session.time}</p>
-            
+
             <h3>Ingressos</h3>
-            {seats.map(seat=>(
+            {seats.map(seat => (
                 <p>Assento {seat}</p>
-            ))}     
-            
+            ))}
+
             <h3>Comprador</h3>
             <p>Nome: {buyer.name}</p>
             <p>CPF: {buyer.cpf}</p>
 
-            <button className="home">
-                Voltar pra Home
-            </button>
+            <Link to= "/">
+                <button className="home">
+                    Voltar pra Home
+                </button>
+            </Link>
         </div>
     );
 }
