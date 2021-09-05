d795dd67472409e790330dff2827a063b42ff7f4
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-09-05 10:58:15 -0300

    Added goBack btn and goHome on logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,48 @@
-import "../Header/Header.css"
+import styled from "styled-components";
+import {MdKeyboardBackspace} from 'react-icons/md';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Header(){
+    let history = useHistory();
     return(
-        <div className="header">
-            <h1>CINEFLEX</h1>
-        </div>
+        <Container>
+            <button onClick={()=>history.goBack()}>
+                <MdKeyboardBackspace/>
+            </button>
+
+            <Link to ="/">
+                <h1>CINEFLEX</h1>
+            </Link>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    height: 67px;
+    background-color: #C3CFD9;
+
+    font-size: 34px;
+    color:#E8833A;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+
+    button {
+        color: #fffFFF;
+        position: absolute;
+        left: 0;
+        margin-left: 10px;
+        background-color: #E8833A;
+        border: none;
+        border-radius: 3px;
+        height: 25px;
+        font-size: 20px;
+    }
+`
+
